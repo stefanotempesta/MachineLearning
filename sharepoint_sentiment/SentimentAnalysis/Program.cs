32a6b7354199a6a1e75dccadc4842fe0f2b2ad78
Stefano Tempesta <stefano.tempesta@outlook.com>
2019-03-01 10:48:49 +1100

    Code refactoring

--- sharepoint_sentiment/SentimentAnalysis/Program.cs
+++ sharepoint_sentiment/SentimentAnalysis/Program.cs
@@ -2,97 +2,97 @@
 using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace CallRequestResponseService
+class Program
 {
-    class Program
+    private readonly string mlServiceEndpoint = ConfigurationManager.AppSettings["MLServiceEndpoint"];
+    private readonly string mlApiKey = ConfigurationManager.AppSettings["MLApiKey"];
+    private readonly string spSiteUrl = ConfigurationManager.AppSettings["SharePointSiteUrl"];
+    private readonly string spUsername = ConfigurationManager.AppSettings["SharePointUsername"];
+    private readonly string spPassword = ConfigurationManager.AppSettings["SharePointPassword"];
+
+    static void Main(string[] args)
     {
-        static readonly string sharepointSiteUrl = ConfigurationManager.AppSettings["SharePointSiteUrl"];
-        static readonly string serviceEndpoint = ConfigurationManager.AppSettings["ServiceEndpoint"];
-        static readonly string apiKey = ConfigurationManager.AppSettings["ApiKey"];
+        var p = new Program();
 
-        static void Main(string[] args)
-        {
-            InvokeMachineLearningService().Wait();
-
-            Console.ReadLine();
-        }
-
-        static async Task InvokeMachineLearningService()
-        {
-            using (var client = new HttpClient())
-            {
-                var scoreRequest = new
-                {
-                    Inputs = new Dictionary<string, StringTable>() {
-                        {
-                            "input1",
-                            new StringTable()
-                            {
-                                ColumnNames = new string[] { "TEXT" },
-                                Values = new string[,] { { "I love SharePoint" } }
-                            }
-                        },
-                    },
-                    GlobalParameters = new Dictionary<string, string>()
-                    {
-                    }
-                };
-                
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
-                client.BaseAddress = new Uri(serviceEndpoint);
-
-                HttpResponseMessage response = await client.PostAsJsonAsync(string.Empty, scoreRequest);
-
-                if (response.IsSuccessStatusCode)
-                {
-                    string result = await response.Content.ReadAsStringAsync();
-                    Console.WriteLine("Result: {0}", result);
-                }
-                else
-                {
-                    Console.WriteLine(string.Format("The request failed with status code: {0}", response.StatusCode));
-                    Console.WriteLine(response.Headers.ToString());
-
-                    string responseContent = await response.Content.ReadAsStringAsync();
-                    Console.WriteLine(responseContent);
-                }
-            }
-        }
-
-        static List<string> RetrieveSharePointComments()
-        {
-            List<string> comments = new List<string>();
-
-            using (SharePointContext sp = new SharePointContext(sharepointSiteUrl, sharepointUsername, sharepointPassword))
-            {
-                List news = sp.List("News");
-                var items = sp.ListItems(news);
-
-                foreach (var item in items)
-                {
-                    var itemComments = sp.Comments(item);
-                    foreach (var itemComment in itemComments)
-                    {
-                        comments.Add(itemComment);
-                    }
-                }
-            }
-
-            return comments;
-        }
-
-        static readonly string sharepointUsername = "stefano";
-        static readonly string sharepointPassword = "Vegas*17";
+        var comments = p.RetrieveSharePointComments();
+        p.InvokeMachineLearningService(comments)
+            .Wait();
+        
+        Console.ReadLine();
     }
 
-    public class StringTable
+    public async Task InvokeMachineLearningService(IEnumerable<string> comments)
+    {
+        // Prepare input data
+        var values = new string[1, comments.Count()];
+        int i = 0;
+        foreach (var comment in comments)
+        {
+            values.SetValue(comment, 1, i++);
+        }
+
+        // Invoke the ML service
+        using (var client = new HttpClient())
+        {
+            client.BaseAddress = new Uri(mlServiceEndpoint);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", mlApiKey);
+
+            var scoreRequest = new
+            {
+                Inputs = new Dictionary<string, StringTable>
+                {
+                    ["inputData"] = new StringTable
+                    {
+                        ColumnNames = new string[] { "TEXT" },
+                        Values = values
+                    }
+                }
+            };
+
+            HttpResponseMessage response = await client.PostAsJsonAsync(string.Empty, scoreRequest);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Result: {0}", result);
+            }
+            else
+            {
+                Console.WriteLine(string.Format("The request failed with status code: {0}", response.StatusCode));
+                Console.WriteLine(response.Headers.ToString());
+
+                string responseContent = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseContent);
+            }
+        }
+    }
+
+    public IList<string> RetrieveSharePointComments()
+    {
+        List<string> comments = new List<string>();
+
+        using (SharePointContext sp = new SharePointContext(spSiteUrl, spUsername, spPassword))
+        {
+            var news = sp.List("News");
+            var items = sp.ListItems(news);
+
+            foreach (var item in items)
+            {
+                var itemComments = sp.Comments(item);
+                comments.AddRange(itemComments);
+            }
+        }
+
+        return comments;
+    }
+
+    private struct StringTable
     {
         public string[] ColumnNames { get; set; }
         public string[,] Values { get; set; }
--- sharepoint_sentiment/SharePointClient/SharePointContext.cs
+++ sharepoint_sentiment/SharePointClient/SharePointContext.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using Microsoft.SharePoint.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace SharePointClient
 {
